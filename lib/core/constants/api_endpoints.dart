56c41a00aa20437891db6790ce1b18e278d7a6fc
Zheembaev Ularbek <legion.ular@gmail.com>
2025-02-28 15:32:46 +0600

    fix

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,9 +1,9 @@
 class ApiEndpoints {
-  static const String baseUrl = 'http://194.87.102.173:4603';
+  static const String baseUrl = 'http://test.com';
   static const String login = '/connect/token';
   static const String refreshToken = '/auth/refresh-token';
   static const String calls = '/calls';
   static const String callCards = '/call-cards';
   static const String hospitals = '/hospitals';
   static const String mkbCodes = '/mkb-codes';
-} 
+}
